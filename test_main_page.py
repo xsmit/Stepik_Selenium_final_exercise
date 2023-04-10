9c17ede4feec8ff4a7c3d5df905bd850db11cc5f
xsmit <55706442+xsmit@users.noreply.github.com>
2023-04-10 17:32:00 +0700

    Transition to login_page from main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,17 @@
 import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 # Запуск: pytest -v --tb=line --language=en test_main_page.py
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     time.sleep(5)
 
 def test_guest_should_see_login_link(browser):
