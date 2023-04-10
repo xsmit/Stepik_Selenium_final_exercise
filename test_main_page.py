a1996f720638ba7d717df055fc2f2256cbcad94e
xsmit <55706442+xsmit@users.noreply.github.com>
2023-04-10 13:20:17 +0700

    Create PageObject lesson2_step2

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,17 @@
+# это лишнее
+import time
 from selenium.webdriver.common.by import By
 
 # Запуск: pytest -v --tb=line --language=en test_main_page.py
 
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    go_to_login_page(browser)
+    # это лишнее
+    time.sleep(5)
