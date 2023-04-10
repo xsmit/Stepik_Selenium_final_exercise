a9c21c802af9944cdea81ad6c8d9676e19ac580e
xsmit <55706442+xsmit@users.noreply.github.com>
2023-04-10 12:09:54 +0700

    Add files in initial state on lesson1_step6

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose site language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    # browser = None
+    print("\nstart browser for test..")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    # Общий вариант запуска
+    # browser = webdriver.Chrome(options=options)
+
+    # Запуск для моего компа
+    chrome_service_executable_path = "C:\\Users\\yury.ageev\\AppData\\Local\\Programs\\ChromeDriver\\chromedriver.exe"
+    chrome_service = Service(executable_path=chrome_service_executable_path)
+    browser = webdriver.Chrome(options=options, service=chrome_service)
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
